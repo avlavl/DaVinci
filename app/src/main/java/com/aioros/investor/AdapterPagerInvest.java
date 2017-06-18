ad780c912c01bc5a38d99fe92e4e1c199534f34d
Aioros Zhang <aioros.zhang@intel.com>
2017-06-18 20:35:53 +0800

    Add invest tab base view

--- app/src/main/java/com/aioros/investor/AdapterPagerInvest.java
+++ app/src/main/java/com/aioros/investor/AdapterPagerInvest.java
@@ -5,7 +5,7 @@ import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -41,8 +41,8 @@ public class AdapterPagerInvest extends PagerAdapter {
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
         View view = mInflater.inflate(R.layout.pager_invest, null);
-        EditText editText = (EditText) view.findViewById(R.id.edittext_pagerinvest);
-        editText.setText(mTabTitles[position]);
+        Button button = (Button) view.findViewById(R.id.button_pagerinvest);
+        button.setText(mTabTitles[position]);
         container.addView(view);
         return view;
     }
