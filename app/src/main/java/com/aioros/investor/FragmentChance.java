429f19753eae27472f886c2fea23515a473a24a8
Aioros Zhang <aioros.zhang@intel.com>
2017-12-27 09:56:36 +0800

    Add chance simple notification

--- app/src/main/java/com/aioros/investor/FragmentChance.java
+++ app/src/main/java/com/aioros/investor/FragmentChance.java
@@ -1,6 +1,11 @@
 package com.aioros.investor;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -12,8 +17,11 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 /**
  * Created by aizhang on 2017/6/7.
@@ -45,11 +53,28 @@ public class FragmentChance extends BaseFragment {
             // 重载消息处理方法，用于接收和处理WorkerThread发送的消息
             @Override
             public void handleMessage(Message msg) {
+                System.out.println("-------------mChanceHandler handle");
                 mMarketDatas = (String[][]) msg.obj;
                 for (int i = 0; i < mStockNames.length; i++) {
                     mBeanStockList.get(i).mStockValue = mMarketDatas[offset + i][1];
                     mBeanStockList.get(i).mStockScope = mMarketDatas[offset + i][2];
                     mBeanStockList.get(i).mStockRatio = mMarketDatas[offset + i][3] + "%";
+                    double value = Double.parseDouble(mBeanStockList.get(i).mStockValue);
+                    if (value < 99.97) {
+                        NotificationManager notificationManager = (NotificationManager) mMainActivity.getSystemService(NOTIFICATION_SERVICE);
+                        Notification.Builder builder = new Notification.Builder(mMainActivity);
+                        builder.setSmallIcon(R.mipmap.ic_app_logo);
+                        builder.setContentTitle(mStockNames[i]); //下拉通知栏标题
+                        builder.setContentText(mStockNames[i] + "当前值" + value + "满足要求"); //下拉通知栏内容
+                        builder.setAutoCancel(true);
+                        builder.setSound(Uri.fromFile(new File("/system/media/audio/notifications/CrystalRing.ogg")));
+                        builder.setDefaults(Notification.DEFAULT_VIBRATE);
+                        Intent intent = new Intent(mMainActivity, MainActivity.class);
+                        PendingIntent contentIntent = PendingIntent.getActivity(mMainActivity, 0, intent, 0);
+                        builder.setContentIntent(contentIntent); //点击跳转的intent
+                        Notification notification = builder.build();
+                        notificationManager.notify((int) System.currentTimeMillis(), notification);
+                    }
                 }
                 mAdapterListView.notifyDataSetChanged();
             }
