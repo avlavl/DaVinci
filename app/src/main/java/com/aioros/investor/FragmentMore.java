c528e6da2f32dc98e1267c3b16d808a91d300e92
zhangxr <dawei_cool@126.com>
2020-07-12 13:16:10 +0800

    Add web tab mult-display support

--- app/src/main/java/com/aioros/investor/FragmentMore.java
+++ app/src/main/java/com/aioros/investor/FragmentMore.java
@@ -1,5 +1,6 @@
 package com.aioros.investor;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import static com.aioros.investor.Constant.FRAGMENT_FLAG_MORE;
 import static com.aioros.investor.R.id.webView;
@@ -18,11 +20,19 @@ import static com.aioros.investor.R.id.webView;
 
 public class FragmentMore extends BaseFragment {
     public WebView mWebView;
+    private TextView mTextView1;
+    private TextView mTextView2;
+    private TextView mTextView3;
+    private TextView mTextView4;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_more, container, false);
         mWebView = (WebView) view.findViewById(webView);
+        //WebView加载web资源
+        mWebView.loadUrl("https://m.yicai.com");
+        mWebView.reload();
+
         //覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
         mWebView.setWebViewClient(new WebViewClient() {
             @Override
@@ -35,11 +45,54 @@ public class FragmentMore extends BaseFragment {
         WebSettings settings = mWebView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        settings.setBuiltInZoomControls(true);
 
-        //WebView加载web资源
-        //mWebView.loadUrl("https://www.jisilu.cn/question/15659");
-        mWebView.loadUrl("https://m.yicai.com");    // http://m.hexun.com/
-        mWebView.reload();
+        mTextView1 = (TextView) view.findViewById(R.id.textViewWeb1);
+        mTextView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextView1.setTextColor(Color.rgb(240, 160, 80));
+                mTextView2.setTextColor(Color.rgb(255, 255, 255));
+                mTextView3.setTextColor(Color.rgb(255, 255, 255));
+                mTextView4.setTextColor(Color.rgb(255, 255, 255));
+                webTabOnClick(v, "http://120.55.49.62/show.php");
+            }
+        });
+        mTextView2 = (TextView) view.findViewById(R.id.textViewWeb2);
+        mTextView2.setTextColor(Color.rgb(240, 160, 80));
+        mTextView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextView1.setTextColor(Color.rgb(255, 255, 255));
+                mTextView2.setTextColor(Color.rgb(240, 160, 80));
+                mTextView3.setTextColor(Color.rgb(255, 255, 255));
+                mTextView4.setTextColor(Color.rgb(255, 255, 255));
+                webTabOnClick(v, "https://m.yicai.com");
+            }
+        });
+        mTextView3 = (TextView) view.findViewById(R.id.textViewWeb3);
+        mTextView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextView1.setTextColor(Color.rgb(255, 255, 255));
+                mTextView2.setTextColor(Color.rgb(255, 255, 255));
+                mTextView3.setTextColor(Color.rgb(240, 160, 80));
+                mTextView4.setTextColor(Color.rgb(255, 255, 255));
+                webTabOnClick(v, "http://m.hexun.com/");
+            }
+        });
+        mTextView4 = (TextView) view.findViewById(R.id.textViewWeb4);
+        mTextView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextView1.setTextColor(Color.rgb(255, 255, 255));
+                mTextView2.setTextColor(Color.rgb(255, 255, 255));
+                mTextView3.setTextColor(Color.rgb(255, 255, 255));
+                mTextView4.setTextColor(Color.rgb(240, 160, 80));
+                webTabOnClick(v, "https://www.jisilu.cn/data/new_stock/#hkipo");
+            }
+        });
+
         return view;
     }
 
@@ -47,5 +100,9 @@ public class FragmentMore extends BaseFragment {
     public void onResume() {
         super.onResume();
         MainActivity.currFragTag = FRAGMENT_FLAG_MORE;
+    }
+
+    private void webTabOnClick(View v, String url) {
+        mWebView.loadUrl(url);
     }
 }
