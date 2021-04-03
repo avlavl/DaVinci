93e9abb4f5395fce818e87987de690e42951bf90
aizhang <aioros.zhang@intel.com>
2021-04-03 15:17:11 +0800

    Modify webpage to add IC

--- app/src/main/java/com/aioros/investor/FragmentMore.java
+++ app/src/main/java/com/aioros/investor/FragmentMore.java
@@ -89,7 +89,7 @@ public class FragmentMore extends BaseFragment {
                 mTextView2.setTextColor(Color.rgb(255, 255, 255));
                 mTextView3.setTextColor(Color.rgb(255, 255, 255));
                 mTextView4.setTextColor(Color.rgb(240, 160, 80));
-                webTabOnClick(v, "https://xueqiu.com/");
+                webTabOnClick(v, "http://flash-quant.com");
             }
         });
 
